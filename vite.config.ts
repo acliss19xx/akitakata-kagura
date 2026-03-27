853e202f73e4c625c3d477ce4491e2b19440871d
Hirofumi Inoue <acliss19xx@gmail.com>
2026-03-28 02:55:27 +0900

    Update base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/git_test/',
+  base: '/akitakata-kagura/',
   plugins: [
     react(),
   ],
